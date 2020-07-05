b30ad4fc8c789167279bba8cb9923b5f42d52750
ryun56 <wakwakugit@gmail.com>
2020-07-06 02:18:03 +0900

    サンプルファイルをコミット

---
+++ RazorPagesMovie/Models/Sample.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace RazorPagesMovie.Models
+{
+    //このファイルはサンプル
+    public class Sample
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        public Sample()
+        {
+        }
+    }
+}
