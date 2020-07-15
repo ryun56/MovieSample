0d4af3066410149aca0c130fa245d827698ffbaa
ryun56 <wakwakugit@gmail.com>
2020-07-16 06:10:37 +0900

    プロパティに検証ロジックを追加

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -8,14 +8,24 @@ namespace RazorPagesMovie.Models
     public class Movie
     {
         public int ID { get; set; }
+
+        [StringLength(60,MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        //入力文字の制限（文字のみを使用、最初の文字は大文字、空白、数字、特殊文字は使用不可）
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
 
         //データベースの通貨とpriceが正しくマッピングするよう以下に注釈
+        [Range(1,100)]
+        [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
     }
